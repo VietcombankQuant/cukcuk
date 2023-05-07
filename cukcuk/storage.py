4b3ac28fe808c5505c54a68af8fe08bb619d4381
Mibu287 <buiducminh287@gmail.com>
2023-05-07 21:33:45 +0700

    Add storage.py file

---
+++ cukcuk/storage.py
@@ -0,0 +1,27 @@
+import sqlalchemy
+from sqlalchemy.orm import Session as SqlSession
+
+
+class Storage:
+    def __init__(self, db_url: str):
+        engine = sqlalchemy.create_engine(url=db_url)
+        self.__session = SqlSession(engine)
+
+    def save(self, objects):
+        # List of objects
+        if type(objects) == list:
+            with self.__session.begin():
+                for obj in objects:
+                    obj.save(self.__session)
+            return
+
+        # One object
+        objects.save(self.__session)
+
+    def load(self, entity, primary_key):
+        with self.__session.begin():
+            return self.__session.get(entity=entity, ident=primary_key)
+
+    def load_all(self, entity):
+        with self.__session.begin():
+            return self.__session.query(entity).all()

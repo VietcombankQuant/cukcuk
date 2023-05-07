eb389ffd5591a387ae90abae8aa115734f4362f8
Mibu287 <buiducminh287@gmail.com>
2023-05-07 22:16:28 +0700

    Add __version__

--- cukcuk/__init__.py
+++ cukcuk/__init__.py
@@ -2,3 +2,5 @@ from .login_session import LoginSession
 from .branch import Branch
 from .invoice import Invoice, InvoiceDetail, InvoicePayment, InvoiceCoupon
 from .storage import Storage
+
+__version__ = "0.2.0"

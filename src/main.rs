e1eb54912f4b90df400c6ec591272388f764df67
Mibu287 <buiducminh287@gmail.com>
2023-05-09 23:32:22 +0700

    Implement main function

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,51 @@
-use std::fmt::format;
+mod model;
 
-use hmac::digest::{KeyInit, Mac, Update};
-use serde::{Deserialize, Serialize};
+use crate::model::{Branch, LoginParam, LoginResponse, ServiceResult};
 
 const API_DOMAIN: &str = "graphapi.cukcuk.vn";
 
-#[derive(Serialize, Deserialize, Debug, Default)]
-struct LoginData {
-    #[serde(rename = "Domain")]
-    pub domain: String,
-
-    #[serde(rename = "AddID")]
-    pub app_id: String,
-
-    #[serde(rename = "LoginTime")]
-    pub login_time: String,
-}
-
-impl LoginData {
-    fn new(domain: &str, app_id: &str) -> Self {
-        let login_time = chrono::Utc::now()
-            .format("%Y-%m-%dT%H:%M:%S%.3fZ")
-            .to_string();
-
-        Self {
-            domain: domain.to_string(),
-            app_id: app_id.to_string(),
-            login_time: login_time,
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, Default)]
-struct LoginParam {
-    #[serde(rename = "Domain")]
-    pub domain: String,
-
-    #[serde(rename = "AddID")]
-    pub app_id: String,
-
-    #[serde(rename = "LoginTime")]
-    pub login_time: String,
-
-    #[serde(rename = "SignatureInfo")]
-    pub signature: String,
-}
-
-impl LoginParam {
-    fn new(domain: &str, app_id: &str, secret_key: &str) -> Self {
-        let login_data = LoginData::new(domain, app_id);
-        let login_data_str = serde_json::to_string(&login_data).unwrap();
-
-        type HmacSha256 = hmac::SimpleHmac<sha2::Sha256>;
-        let mut mac = HmacSha256::new(secret_key.as_bytes());
-        mac.update(login_data_str.as_bytes());
-        let signature = mac.finalize().into_bytes();
-        let signature = format!("{:x}", signature);
-
-        Self {
-            domain: login_data.domain,
-            app_id: login_data.app_id,
-            login_time: login_data.login_time,
-            signature,
-        }
-    }
-}
+#[derive(serde::Deserialize)]
+struct Foo {}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let login_url = format!("https://{}/api/Account/Login");
     let api_client = reqwest::Client::new();
 
+    // Get access token
+    let login_url = format!("https://{}/api/Account/Login", API_DOMAIN);
     let login_param = LoginParam::new(
         "bownb",
         "CUKCUKOpenPlatform",
-        "7e1612bffb97ce9bc4c5c47e5c972ca566c951e030ae828569533fec73ba41f5",
+        "cd76e8fe462e8af36d137cd78a522c2d83bbd0861f2905e9b3180165c0d53df0",
     );
 
-    println!("Signature: {}", login_param.signature);
+    let request = api_client.post(&login_url).json(&login_param).build()?;
+    println!("request {:?}", request.body().unwrap());
 
-    //let resp = api_client.get(&login_url).json(&login_data).send().await?;
+    let resp = api_client.execute(request).await?;
+    let message = resp.text().await?;
+    let result: ServiceResult<LoginResponse> = serde_json::from_str(&message)?;
+    let access_token = match result.data() {
+        None => "",
+        Some(data) => data.token(),
+    };
+    println!("access_token {}", access_token);
+
+    let authorization = format!("Bearer {}", access_token);
+
+    // Get branches
+    let branch_url = format!("https://{}/api/v1/branchs/all", API_DOMAIN);
+    let resp = api_client
+        .get(branch_url)
+        .header("CompanyCode", "bownb")
+        .header("Authorization", authorization)
+        .send()
+        .await?;
+
+    let message = resp.text().await?;
+    let results: ServiceResult<Vec<Branch>> = serde_json::from_str(&message)?;
+
+    println!("Branches: {:#?}", results);
 
     Ok(())
 }
